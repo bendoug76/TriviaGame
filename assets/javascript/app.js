953d9e4707076afa328b4284e7bc0096399bc2b3
Benjamin Lee <benjamindlee4@gmail.com>
2019-09-25 16:51:41 -0500

    remade divs added questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+// start button to begin timer and quiz
+// questions laid out with multiple choice answers
+//either timer ends or player uses stop button
+// display results and message
+
+var questions = [
+    {
+        question: "Who palyed lead guitar for Pantera?",
+        answers: {
+            a: "Dimebag Darrell",
+            b: "Zakk Wylde",
+            c: "Kirk Hammett"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "In a fight between Lemmy and God, who would win?",
+        answers: {
+            a: "God",
+            b: "Lemmy",
+            c: "Trick question, Lemmy is God"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What band appeard as 'Wildlings' in Game of Thrones?",
+        answers:{
+            a: "Sepultura",
+            b: "Mastodon",
+            c: "Lamb of God",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Where is Ozzy Osbourne from?",
+        answers:{
+            a: "Birmingham, AL",
+            b: "Birmingham, UK",
+            c: "Birmingham, MI",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "Who sang the song 'Satan is My Master'?",
+        answers:{
+            a: "Gorgoroth",
+            b: "Dimmu Borgir",
+            c: "Ben Folds Five",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What is the answer to the ultimate question of life, the universe, and everything",
+        answers:{
+            a: "42",
+            b: "Happiness, love, and family",
+            c: "Beer",
+        },
+        correctAnswer: "a"
+    },
+]
+
+
