0210645a5f21fb43f65050ee90ef544fc99c756c
Benjamin Lee <benjamindlee4@gmail.com>
2019-09-26 20:56:12 -0500

    added js and new html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,61 +3,133 @@
 //either timer ends or player uses stop button
 // display results and message
 
-var questions = [
-    {
-        question: "Who palyed lead guitar for Pantera?",
-        answers: {
-            a: "Dimebag Darrell",
-            b: "Zakk Wylde",
-            c: "Kirk Hammett"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "In a fight between Lemmy and God, who would win?",
-        answers: {
-            a: "God",
-            b: "Lemmy",
-            c: "Trick question, Lemmy is God"
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "What band appeard as 'Wildlings' in Game of Thrones?",
-        answers:{
-            a: "Sepultura",
-            b: "Mastodon",
-            c: "Lamb of God",
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Where is Ozzy Osbourne from?",
-        answers:{
-            a: "Birmingham, AL",
-            b: "Birmingham, UK",
-            c: "Birmingham, MI",
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Who sang the song 'Satan is My Master'?",
-        answers:{
-            a: "Gorgoroth",
-            b: "Dimmu Borgir",
-            c: "Ben Folds Five",
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "What is the answer to the ultimate question of life, the universe, and everything",
-        answers:{
-            a: "42",
-            b: "Happiness, love, and family",
-            c: "Beer",
-        },
-        correctAnswer: "a"
-    },
-]
+var counter = 30;
+var currentQuestion = 0;
+var score = 0;
+var lost = 0;
+var timer;
+
+function nextQuestion(){
+    const isQuestionOver = (triviaQuestions.length - 1) === currentQuestion;
+    if(isQuestionOver){
+        console.log("Game Over!");
+        displayResult();
+    } else {
+
+    currentQuestion++;
+    loadQuestion();
+    
+    }
+
+}
+function timeUp(){
+    clearInterval(timer);
+
+    lost++;
+    preloadImage("lost");
+    setTimeout(nextQuestion, 3 * 1000);
+}
+
+function countDown(){
+    counter--;
+    $("#time").html("Time: " + counter);
+
+    if (counter === 0){
+        timeUp();
+    }
+}
+
+function loadQuestion(){
+    counter = 30;
+    timer = setInterval(countDown, 1000);
+
+
+    const question = triviaQuestions[currentQuestion].question; 
+    const answers = triviaQuestions[currentQuestion].answers; 
+
+
+    $("#timer").html("Timer: " + counter);
+    $("#game").html(`
+        <h4>${question}</h4>
+        ${loadChoices(answers)}
+        
+    `);
+    
+}
+
+function loadChoices(answers){
+    var result = "";
+    for (let i = 0; i < answers.length; i++) {
+        result += `<p class="choice" data-answer="${answers[i]}">${answers[i]}</p>`;
+    }
+    return result;  
+}
+
+$(document).on("click", ".choice", function(){
+    clearInterval(timer);
+    const selectedAnswer =$(this).attr("data-answer");
+    const correctAnswer = triviaQuestions[currentQuestion].correctAnswer;
+
+    if(correctAnswer === selectedAnswer){
+        score++;
+        console.log("winner")
+        preloadImage("win");
+        setTimeout(nextQuestion, 3 * 1000);
+    } else {
+        lost++;
+        console.log("loser!");
+        preloadImage("lost");
+        setTimeout(nextQuestion, 3 * 1000);
+    }
+    console.log("stuff", selectedAnswer);
+});
+
+function displayResult() {
+    const result = `
+    <p>You Got ${score} Question (s) Right!!</p>
+    <p>You Got ${lost} Question (s) Wrong. SAD FACE</p>
+    <p>Total Questions ${triviaQuestions.length} Question (s) Right!!</p>
+    <button class "btn btn-dark" id="reset">Reset Game</button>
+    `;
+    $("#game").html(result);
+}
+
+$(document).on("click", "#reset", function(){
+    location.reload(true);
+});
+
+function randomImage(images) {
+    const random = Math.floor(Math.random()* images.length);
+    const randomImage = images[random];
+    return randomImage;
+}
+
+function preloadImage(status) {
+    const correctAnswer = triviaQuestions[currentQuestion].correctAnswer;
+
+    if(status === "win"){
+        $("#game").html(`
+            <p class="preload-image">Correct!</p>
+            <p class="preload-image">The correct answer is: <b>${correctAnswer}</b></p>
+            <img src="${randomImage(rightAnswers)}" />
+        
+            `);
+    } else{
+        $("#game").html(`
+            <p class="preload-image">Wrong!</p>
+            <p class="preload-image">The correct answer is: <b>${correctAnswer}</b></p>
+            <img src="${randomImage(wrongAnswers)}" />
+            `);
+
+    }
+}
+
+
+$("#start").click(function() {
+    $("#start").remove();
+    $("#time").html(counter);
+    loadQuestion();
+});
+ 
 
 
---
+++ assets/javascript/questions.js
@@ -0,0 +1,67 @@
+var triviaQuestions = [
+    {
+        question: "Who palyed lead guitar for Pantera?",
+        answers: [
+             "Dimebag Darrell",
+             "Zakk Wylde",
+             "Kirk Hammett"],
+        
+        correctAnswer: "Dimebag Darrell"
+    },
+    {
+        question: "In a fight between Lemmy and God, who would win?",
+        answers:[ 
+             "God",
+             "Lemmy",
+             "Trick question, Lemmy is God"],
+        
+             correctAnswer: "Trick question, Lemmy is God"
+    },
+    {
+        question: "What band appeard as 'Wildlings' in Game of Thrones?",
+        answers:[
+             "Sepultura",
+             "Mastodon",
+             "Lamb of God"],
+        
+        correctAnswer: "Mastodon"
+    },
+    {
+        question: "Where is Ozzy Osbourne from?",
+        answers:[
+             "Birmingham, AL",
+             "Birmingham, UK",
+             "Birmingham, MI"],
+        
+        correctAnswer: "Birmingham, UK"
+    },
+    {
+        question: "Who sang the song 'Satan is My Master'?",
+        answers:[
+             "Gorgoroth",
+             "Dimmu Borgir",
+             "Ben Folds Five"],
+        
+        correctAnswer: "Ben Folds Five"
+    },
+    {
+        question: "What is the answer to the ultimate question of life, the universe, and everything",
+        answers:[
+             "42",
+             "Happiness, love, and family",
+             "Beer"],
+        
+        correctAnswer: "42"
+    },
+]
+var rightAnswers = [
+    "assets/images/right1.gif",
+    "assets/images/right2.gif",
+    "assets/images/right3.gif",
+];
+
+var wrongAnswers = [
+    "assets/images/wrong1.gif",
+    "assets/images/wrong2.gif",
+    "assets/images/wrong3.gif",
+];
